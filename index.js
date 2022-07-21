f121620448af3dec97f1157257e4d676bb45557a
IvanZezyukin <i.zezukin@gmail.com>
2022-07-21 17:08:20 +0300

    modified to use as a function in the yandex functions

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 const AWS = require('aws-sdk');
 
-// const ID = 'YCAJEhvD8dniQA61ctI-BA-6D';
-// const SECRET = 'YCP9ZCUC4Pup4PW1EEn94DYIoOUvsbenObDUptcA';
-// const BUCKET_NAME = 'emails';
-// const REGION = '';
+module.exports.handler = async function (event, context) {
+  email = event.queryStringParameters.email;
 
-const s3 = new AWS.S3({
-  accessKeyId: 'YCAJEhvD8dniQA61ctI-BA-6D',
-  secretAccessKey: 'YCP9ZCUC4Pup4PW1EEn94DYIoOUvsbenObDUptcA',
-  endpoint: 'storage.yandexcloud.net',
-  region: 'ru-central1'
-})
-
-const uploadFile = (fileName) => {
-  const params = {
-    Bucket: 'emails',
-    Key: fileName,
-    Body: ''
-  };
-  s3.upload(params, (err, data) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`File uploaded successfully`);
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.ID,
+    secretAccessKey: process.env.KEY,
+    endpoint: 'storage.yandexcloud.net',
+    region: 'ru-central1'
   })
-}
 
-uploadFile('helloworld');
+  const uploadFile = (fileName) => {
+    const params = {
+      Bucket: 'emails',
+      Key: fileName,
+      Body: ''
+    };
+    s3.upload(params, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`File uploaded successfully`);
+    });
+  };
+
+  uploadFile(email);
+
+  return {
+    statusCode: 200,
+    body: 'Email saved'
+  };
+
+};
